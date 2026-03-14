3e83c0db42afbb6f213acf44bccbe2ecb06a3438
ATRussell63 <atrussell63@gmail.com>
2026-03-13 20:14:37 -0400

    disable the test endpoints. woops!

--- app/views/test_view.py
+++ app/views/test_view.py
@@ -26,37 +26,37 @@ def fetch_all_jewels(query):
         return results
 
 
-@test_view.route('/test', methods=['GET', 'POST'])
-def view_test():
-    """
-        Return all jewels in the db for ui testing purposes.
-    """
+# @test_view.route('/test', methods=['GET', 'POST'])
+# def view_test():
+#     """
+#         Return all jewels in the db for ui testing purposes.
+#     """
     
-    bogus_search_params = SearchRequest(
-        jewel_type='Militant Faith',
-        seed=12345,
-        general='Borgus',
-        mf_mods=['1% reduced Mana Cost of Skills per 10 Devotion',
-                 '+2% to all Elemental Resistances per 10 Devotion']
-    )
+#     bogus_search_params = SearchRequest(
+#         jewel_type='Militant Faith',
+#         seed=12345,
+#         general='Borgus',
+#         mf_mods=['1% reduced Mana Cost of Skills per 10 Devotion',
+#                  '+2% to all Elemental Resistances per 10 Devotion']
+#     )
 
-    response = {}
-    try:
-        q = query_fetch_all_jewels(bogus_search_params)
-        data = fetch_all_jewels(q)
-        response['results'] = format_jewel_search_results(data, bogus_search_params)
+#     response = {}
+#     try:
+#         q = query_fetch_all_jewels(bogus_search_params)
+#         data = fetch_all_jewels(q)
+#         response['results'] = format_jewel_search_results(data, bogus_search_params)
 
-    except Exception as e:
-        return jsonify({'error': f'{e}'}), 500
+#     except Exception as e:
+#         return jsonify({'error': f'{e}'}), 500
 
-    return jsonify(response), 200
+#     return jsonify(response), 200
 
 
 # TODO - delete this shit later or something
-@test_view.route('/test/manual', methods=['GET', 'POST'])
-def view_test_manual_data():
-    """
-        Return a manual subset of the data.
-    """
-    from app.views.view_helpers.view_test_view_helpers import all_data_response
-    return jsonify(all_data_response), 200
+# @test_view.route('/test/manual', methods=['GET', 'POST'])
+# def view_test_manual_data():
+#     """
+#         Return a manual subset of the data.
+#     """
+#     from app.views.view_helpers.view_test_view_helpers import all_data_response
+#     return jsonify(all_data_response), 200
